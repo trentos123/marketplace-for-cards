6c975b23a1b7f798e50b4e8daebebfa1a24fd67f
trentos123 <trentos123@example.com>
2026-06-04 15:00:04 +0000

    fix urls to match stripe views

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,14 +1,22 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("create/", views.create, name="create"),
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("cart/", views.cart, name="cart"),
+    path("card/<int:pk>/", views.detail, name="detail"),
 
-    # LOGIN (THIS IS IMPORTANT)
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    # CART
+    path("add/<int:pk>/", views.add_to_cart, name="add_to_cart"),
+    path("cart/", views.cart, name="cart"),
+    path("remove/<int:pk>/", views.remove_from_cart, name="remove_from_cart"),
+
+    # CHECKOUT
+    path("checkout/", views.checkout, name="checkout"),
+    path("payment-success/", views.payment_success, name="payment_success"),
+
+    # WEBHOOK
+    path("stripe/webhook/", views.stripe_webhook, name="stripe_webhook"),
+
+    # AUTH
+    path("register/", views.register, name="register"),
 ]
